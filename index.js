20e0b2292bc33b9b4e1130b34075b9dd4eff29ef
Emma Fewer <emmafewer20@gmail.com>
2020-09-17 14:30:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+var musicianString;
+var musicianInstrument=[];
+for(var i=0; i<musicians.length; i++){
+musicianString= musicians[i] + " plays " + instruments[i];
+musicianInstrument[i]=musicianString;
+}
+return musicianInstrument;
+}
+
+function johnLennonFacts(facts) {
+var newArray = []; 
+let i = 0;
+
+while (i<facts.length) {
+newArray=(`${facts} + !!!`);
+i++;
+}
+ return newArray 
+}
